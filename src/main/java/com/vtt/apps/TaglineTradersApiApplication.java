e3c09aebc5f798a753ee3f60c4b51c74029b2d37
= <=>
2021-04-09 18:08:14 +0530

    Logging to External File

--- src/main/java/com/vtt/apps/TaglineTradersApiApplication.java
+++ src/main/java/com/vtt/apps/TaglineTradersApiApplication.java
@@ -2,20 +2,13 @@ package com.vtt.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class TaglineTradersApiApplication extends SpringBootServletInitializer{
+public class TaglineTradersApiApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaglineTradersApiApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(TaglineTradersApiApplication.class);
 	}
 
 }
